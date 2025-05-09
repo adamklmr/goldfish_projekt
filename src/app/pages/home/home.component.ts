afdd8cda3a572781db68e972c47edf97ef01b326
Kalmár Ádám <h363725@stud.u-szeged.hu>
2025-05-09 23:34:27 +0200

    fooldal megjelenites

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { CurrencyPipePipe } from '../../shared/pipes/currency.pipe.pipe';
 import { DateFormatterPipe } from '../../shared/pipes/date.pipe';
 import { Event } from '../../shared/models/Event';
+import { EventService } from '../../shared/services/event.service';
+import { ProductService } from '../../shared/services/product.service';
 
 
 
@@ -16,17 +18,39 @@ import { Event } from '../../shared/models/Event';
     MatCardModule,
     MatButtonModule,
     CurrencyPipePipe,
-    
+    DateFormatterPipe
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   products: Product[] = [];
   events: Event[] = [];
 
+  constructor(
+    private eventService: EventService,
+    private productService: ProductService
+  ) {}
+  ngOnInit(): void {
+    this.eventService.getAllEvents().subscribe({
+      next: (data) => {
+        this.events = data;
+      },
+      error: (err) => {
+        console.error('Error loading events:', err);
+      }
+    });
+    this.productService.getAllProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (err) => {
+        console.error('Error loading products:', err);
+      }
+    });
+  }
 
-  hasStarted(eventStartDate: Date): boolean {
+  hasStarted(eventStartDate: string): boolean {
     const now = new Date();
     return new Date(eventStartDate) < now;
   }
